0e002900a12a293e97738d6e176b0b04925e0bf4
sarkarinaukariresult <43061551+ashooooook@users.noreply.github.com>
2026-07-30 00:08:24 +0530

    Use native pickup time picker

--- app/page.tsx
+++ app/page.tsx
@@ -2735,37 +2735,17 @@ export default function Home() {
               </label>
               <label className="booking-field">
                 <span>Pick Up Time</span>
-                <select
+                <input
                   value={pickupTime}
                   onChange={(event) => {
                     setPickupTime(event.target.value);
                     setShowVehicleStep(false);
                     setBookingView("home");
                   }}
-                >
-                  {[
-                    "06:00",
-                    "07:00",
-                    "08:00",
-                    "09:00",
-                    "10:00",
-                    "11:00",
-                    "12:00",
-                    "13:00",
-                    "14:00",
-                    "15:00",
-                    "16:00",
-                    "17:00",
-                    "18:00",
-                    "19:00",
-                    "20:00",
-                    "21:00",
-                  ].map((time) => (
-                    <option key={time} value={time}>
-                      {time}
-                    </option>
-                  ))}
-                </select>
+                  type="time"
+                  step="60"
+                  required
+                />
               </label>
               <label className="booking-field km-field">
                 <span>KM</span>
